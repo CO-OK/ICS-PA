32f23dc066579aae5bff2a47272f3f3cccf17857
tracer-ics2017 <tracer@njuics.org>
2021-03-01 13:32:28 +0800

    >  compile 1410576 v Linux v-pc 5.4.97-1-MANJARO #1 SMP PREEMPT Wed Feb 10 10:24:32 UTC 2021 x86_64 GNU/Linux  13:32:28 up  2:49,  3 users,  load average: 0.36, 0.43, 0.42 b81455a0f45a2586311c42f541a2ed6fa06e90f

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -186,6 +186,8 @@ static int cmd_mem(char*args)
     printf("arg wrong!\naddr must be hex format.\nrange must be dec format.\n");
     return 0;
   }
+  printf("addr=%s\n",addr);
+  printf("len=%s\n",len);
   for(int i=1;i<=atoi(len);i++)
   {
     printf("0x%X\t0x%X\n",atoi(addr)+i-1,paddr_read(atoi(addr)+i-1,1));
