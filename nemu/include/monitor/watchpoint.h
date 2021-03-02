1cb943acd2b982c77638062719e51e9ecca58b3c
tracer-ics2017 <tracer@njuics.org>
2021-03-02 22:51:52 +0800

    >  compile 1410576 v Linux v-pc 5.4.100-1-MANJARO #1 SMP PREEMPT Tue Feb 23 15:31:04 UTC 2021 x86_64 GNU/Linux  22:51:52 up 7 min,  3 users,  load average: 0.13, 0.77, 0.55 2a856ae65e4804f6f1860b6f6b9697e3db79611b

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -6,10 +6,13 @@
 typedef struct watchpoint {
   int NO;
   struct watchpoint *next;
-
+  int is_busy;
   /* TODO: Add more members if necessary */
 
 
 } WP;
 
+WP* new_wp();
+void free_wp(WP* wp);
+WP* finr_prev(WP* wp);
 #endif
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -10,14 +10,56 @@ void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
-    wp_pool[i].next = &wp_pool[i + 1];
+    //wp_pool[i].next = &wp_pool[i + 1];
+    wp_pool[i].next=NULL;
+    wp_pool[i].is_busy=0;
   }
   wp_pool[NR_WP - 1].next = NULL;
 
   head = NULL;
   free_ = wp_pool;
 }
-
+WP* new_wp()
+{
+  for(int i=0;i<NR_WP;i++)
+  {
+    if(wp_pool[i].is_busy==0)
+    {
+      wp_pool[i].is_busy=1;
+      return &wp_pool[i];
+    }
+      
+  }
+  return NULL;
+}
+WP* finr_prev(WP* wp)
+{
+  for(int i=0;i<NR_WP;i++)
+  {
+    if(wp_pool[i].next==wp)
+      return &wp_pool[i];
+  }
+  return NULL;
+}
+void free_wp(WP* wp)
+{
+  if(wp==head)
+  {
+    head=wp->next;
+    
+  }
+  else if(wp->next==NULL)
+  {
+    finr_prev(wp)->next=NULL;
+  }
+  else
+  {
+    WP*prev=finr_prev(wp);
+    prev->next=wp->next;
+  }
+  wp->next=NULL;
+  wp->is_busy=0;
+}
 /* TODO: Implement the functionality of watchpoint */
 
 
