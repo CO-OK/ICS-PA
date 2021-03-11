94484b3bb273706de450b53e778ee45e9340c293
tracer-ics2017 <tracer@njuics.org>
2021-03-11 21:10:54 +0800

    >  compile 1410576 v Linux v-pc 5.4.101-1-MANJARO #1 SMP PREEMPT Fri Feb 26 11:18:55 UTC 2021 x86_64 GNU/Linux  21:10:54 up  1:09,  3 users,  load average: 0.52, 0.55, 0.56 3786f45314ba35fbe86169aa83ccea5cad8458da

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -21,6 +21,43 @@ make_EHelper(xor) {
 make_EHelper(or) {
   //TODO();
   //decoding.dest
+  if(decoding.src.type==OP_TYPE_REG)
+  {
+    switch (decoding.src2.type)
+    {
+      case OP_TYPE_MEM:{
+        uint32_t val = paddr_read(decoding.src2.addr,32);
+        reg_l(decoding.src.reg) |= val;
+        break;
+      }
+      case OP_TYPE_REG:{
+        reg_l(decoding.src.reg) |= reg_l(decoding.src2.reg);
+        break;
+      }
+      case OP_TYPE_IMM:{
+        reg_l(decoding.src.reg) |= decoding.src2.imm;
+        break;
+      }
+    }
+  }
+  else if(decoding.src.type==OP_TYPE_MEM)
+  {
+    uint32_t val = paddr_read(decoding.src.addr,32);
+    switch(decoding.src2.type)
+    {
+      case OP_TYPE_REG:{
+        val|= reg_l(decoding.src2.reg);
+        break;
+      }
+      case OP_TYPE_IMM:{
+        uint32_t val2 = paddr_read(decoding.src2.addr,32);
+        val |= val2;
+        break;
+      }
+    }
+    paddr_write(decoding.src.addr,32,val);
+  }
+  
   print_asm_template2(or);
 }
 
