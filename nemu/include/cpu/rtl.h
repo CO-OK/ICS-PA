f718cbaa17960cab7c8a523aca2d1e32fa844c00
tracer-ics2017 <tracer@njuics.org>
2021-03-15 21:21:22 +0800

    >  compile 1410576 v Linux v-pc 5.4.101-1-MANJARO #1 SMP PREEMPT Fri Feb 26 11:18:55 UTC 2021 x86_64 GNU/Linux  21:21:22 up  2:08,  3 users,  load average: 1.10, 0.98, 0.95 cda00e894e37193d74416a7260c2d0a22094c384

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -168,7 +168,7 @@ static inline void rtl_pop(rtlreg_t* dest) {
   // esp <- esp + 4
   //TODO();
   //operand_write(&id_dest->reg,)
-  *dest = paddr_read(cpu.esp,1);
+  *dest = paddr_read(cpu.esp,4);
   
   //operand_write(dest,&temp);
   cpu.esp += 4;
