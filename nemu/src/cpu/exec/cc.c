2cd2fbade439359902947b007085f2b7d2db4165
tracer-ics2017 <tracer@njuics.org>
2021-03-17 23:07:58 +0800

    >  compile 1410576 v Linux v-pc 5.4.101-1-MANJARO #1 SMP PREEMPT Fri Feb 26 11:18:55 UTC 2021 x86_64 GNU/Linux  23:07:58 up  4:32,  3 users,  load average: 0.66, 0.67, 0.65 c08ee202c4116ebe1110c9d686919bf3e904785e

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -19,8 +19,9 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {//94
     case CC_E:{
       rtl_get_ZF(&t0);
       printf("666\n");
-      if(t0)//等于0说明ZF=0
+      if(t0==0)//等于0说明ZF=0
       {
+        printf("777\n");
         *dest = (char)0;
       }
       else
