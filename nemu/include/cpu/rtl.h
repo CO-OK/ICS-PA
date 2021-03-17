7ae5f4e43b03aac8910c4c3abb3b9cdf84b3b2b8
tracer-ics2017 <tracer@njuics.org>
2021-03-17 21:41:05 +0800

    >  compile 1410576 v Linux v-pc 5.4.101-1-MANJARO #1 SMP PREEMPT Fri Feb 26 11:18:55 UTC 2021 x86_64 GNU/Linux  21:41:05 up  3:05,  3 users,  load average: 0.48, 0.66, 0.74 7f382ff06733fe4197ed82cd2e37179cdd657a8c

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -199,8 +199,8 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-  rtl_shli(&t3,result,32-width);
-  rtl_shri(&t0,&t1,32-width);
+  rtl_shli(&t3,result,32-width*8);
+  rtl_shri(&t0,&t1,32-width*8);
   if(t0==0)
   {
     rtl_set_ZF(&eflag_ZF);
