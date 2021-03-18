98817877349e6902ae846590ae35070bc3c0ca9a
tracer-ics2017 <tracer@njuics.org>
2021-03-18 11:02:40 +0800

    >  compile 1410576 v Linux v-pc 5.4.101-1-MANJARO #1 SMP PREEMPT Fri Feb 26 11:18:55 UTC 2021 x86_64 GNU/Linux  11:02:40 up  2:15,  3 users,  load average: 0.42, 0.47, 0.59 d3810776aa852dd16e2822e877a079e23d22445d

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -10,9 +10,7 @@ make_EHelper(jmp) {
 make_EHelper(jcc) {
   // the target address is calculated at the decode stage
   uint8_t subcode = decoding.opcode & 0xf;
-  t2=0;
   rtl_setcc(&t2, subcode);
-  printf("t2=%d\n",t2);
   decoding.is_jmp = t2;
 
   print_asm("j%s %x", get_cc_name(subcode), decoding.jmp_eip);
