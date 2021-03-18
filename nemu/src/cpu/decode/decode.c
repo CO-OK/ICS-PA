e7a5d8859c7ec72f0fc9da7015ac7fb2b376203d
tracer-ics2017 <tracer@njuics.org>
2021-03-18 22:10:12 +0800

    >  compile 1410576 v Linux v-pc 5.4.101-1-MANJARO #1 SMP PREEMPT Fri Feb 26 11:18:55 UTC 2021 x86_64 GNU/Linux  22:10:12 up  1:49,  3 users,  load average: 0.56, 0.64, 0.76 7b00a22cd62cfbfe8e68b7286e2c9ae869419972

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -265,9 +265,10 @@ make_DHelper(J) {
   // the target address can be computed in the decode stage
   //printf("simm=%08X\neip=%08X\n",id_dest->simm,*eip);
   printf("width=%d\n",id_dest->width);
+  printf("eip=%08X\n",*eip);
   if(id_dest->width==1)//宽度为1时相对地址相加以后进位要丢弃
   {
-    char last_two_byte = (char)eip;
+    char last_two_byte = (char)(*eip);
     printf("byte=%02X\n",last_two_byte);
     char byte_res = (char)(last_two_byte + (char)id_dest->simm);
     printf("final=%02X\n",byte_res);
