5124a52b4efa73e4073379d953e02304c83372fd
tracer-ics2017 <tracer@njuics.org>
2021-03-22 22:56:46 +0800

    >  compile 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  22:56:46 up  3:39,  4 users,  load average: 0.70, 0.58, 0.54 9721896d5b18c359cee43ce4d2f34f38da02fad5

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -276,13 +276,14 @@ make_DHelper(J) {
     printf("simm=%08X\n",id_dest->simm);
     printf("imm=%08X\n",id_dest->imm);
     printf("val=%08X\n",id_dest->val);
-    rtl_sext(&t0,&id_dest->val,id_dest->width);
-    printf("t0=%08X\n",t0);
-    printf("res=%08X\n",t0+*eip);
+    rtlreg_t t;
+    rtl_sext(&t,&id_dest->val,id_dest->width);
+    printf("t=%08X\n",t);
+    printf("res=%08X\n",t+*eip);
     unsigned char temp = (unsigned char)(id_dest->simm) + (unsigned char)(*eip);
     //temp &= 0b11111011;//进位不光丢弃，进位上的值也得置0
     printf("temp=%08X\n",temp);
-    decoding.jmp_eip = tmp_eip + (unsigned char)temp;
+    decoding.jmp_eip = t+*eip;
   }
   else if(id_dest->width==2)//宽度为2时同理
   {
