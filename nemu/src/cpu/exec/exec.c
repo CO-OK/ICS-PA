1b956df139cf325ce49a1cb16fe513b3f6e78e14
tracer-ics2017 <tracer@njuics.org>
2021-03-22 20:52:24 +0800

    >  compile 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  20:52:24 up  1:34,  4 users,  load average: 1.12, 0.72, 0.75 c34d82b40a2f2423a2f4e3828c718486c8d2e4

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -75,7 +75,7 @@ make_group(gp7,
 opcode_entry opcode_table [512] = {
   /* 0x00 */	EMPTY, EMPTY, EMPTY, IDEXW(E2G,add,0),//03 add Gv,Ev
   /* 0x04 */	EMPTY, /*IDEX(mov_I2r,add)*/EMPTY, EMPTY, EMPTY,//05--> ADD EAX, Iv IDEX(mov_I2r,ADD)
-  /* 0x08 */	/*IDEX(G2E,or)*/EMPTY, EMPTY, IDEX(E2G,or), EMPTY,//08-->0D or
+  /* 0x08 */	/*IDEX(G2E,or)*/EMPTY, EMPTY, IDEXW(E2G,or,1), EMPTY,//08-->0D or
   /* 0x0c */	EMPTY, EMPTY, EMPTY, EX(2byte_esc),
   /* 0x10 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x14 */	EMPTY, EMPTY, EMPTY, EMPTY,
