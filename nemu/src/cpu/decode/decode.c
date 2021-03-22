8e0ab93136645785908097e3bb550f291a72dc04
tracer-ics2017 <tracer@njuics.org>
2021-03-22 23:18:15 +0800

    >  compile 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  23:18:15 up  4:00,  4 users,  load average: 0.78, 0.83, 0.84 40e6ddfcd8c0c4c5142a8a06a861bcc2f451f46

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -268,31 +268,35 @@ make_DHelper(J) {
   //printf("eip=%08X\n",*eip);
   if(id_dest->width==1)//宽度为1时相对地址相加以后进位要丢弃
   {
-    printf("eip=%08X\n",*eip);
-    vaddr_t tmp_eip = *eip;
-    tmp_eip = tmp_eip >> 8;
-    tmp_eip = tmp_eip << 8;
-    printf("tmp_eip=%08X\n",tmp_eip);
-    printf("simm=%08X\n",id_dest->simm);
-    printf("imm=%08X\n",id_dest->imm);
-    printf("val=%08X\n",id_dest->val);
+    //printf("eip=%08X\n",*eip);
+    //vaddr_t tmp_eip = *eip;
+    //tmp_eip = tmp_eip >> 8;
+    //tmp_eip = tmp_eip << 8;
+    //printf("tmp_eip=%08X\n",tmp_eip);
+    //printf("simm=%08X\n",id_dest->simm);
+    //printf("imm=%08X\n",id_dest->imm);
+    //printf("val=%08X\n",id_dest->val);
     rtlreg_t t;
     rtl_sext(&t,&id_dest->val,id_dest->width);
-    printf("t=%08X\n",t);
-    printf("res=%08X\n",t0+*eip);
-    unsigned char temp = (unsigned char)(id_dest->simm) + (unsigned char)(*eip);
+    //printf("t=%08X\n",t);
+    //printf("res=%08X\n",t0+*eip);
+    //unsigned char temp = (unsigned char)(id_dest->simm) + (unsigned char)(*eip);
     //temp &= 0b11111011;//进位不光丢弃，进位上的值也得置0
-    printf("temp=%08X\n",temp);
+    //printf("temp=%08X\n",temp);
     decoding.jmp_eip = t+*eip;
   }
   else if(id_dest->width==2)//宽度为2时同理
   {
     //printf("width=%d\n",2);
-    vaddr_t tmp_eip = *eip;
+    /*vaddr_t tmp_eip = *eip;
     tmp_eip = tmp_eip >> 16;
     tmp_eip = tmp_eip << 16;
     short temp = id_dest->simm + *eip;
-    decoding.jmp_eip = tmp_eip + (short)temp;
+    decoding.jmp_eip = tmp_eip + (short)temp;*/
+    rtlreg_t t;
+    rtl_sext(&t,&id_dest->val,id_dest->width);
+    decoding.jmp_eip = t+*eip;
+    decoding.jmp_eip &= 0xffff;
   }
   else if(id_dest->width==4)
   {
