6a689fedafd23e6da2d29f931f5c9850db2d636e
tracer-ics2017 <tracer@njuics.org>
2021-03-22 21:47:46 +0800

    >  compile 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  21:47:46 up  2:30,  4 users,  load average: 0.88, 0.54, 0.47 59af1820e7a2da628be97772b268ffc0b369dcce

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -188,7 +188,7 @@ uint32_t eval(int head,int tail)
     {
       int val1=eval(head,op-1);
       int val2=eval(op+1,tail);
-      printf("val1=%d,val2=%d\n",val1,val2);
+      //printf("val1=%d,val2=%d\n",val1,val2);
       switch(tokens[op].str[0])
       {
         case '+':{
@@ -233,7 +233,7 @@ uint32_t eval(int head,int tail)
       {
         val=hex2dec(tokens[op+1].str);
       }
-      printf("val=%d\n",val);
+      //printf("val=%d\n",val);
       return paddr_read(val,1);
     }
     
