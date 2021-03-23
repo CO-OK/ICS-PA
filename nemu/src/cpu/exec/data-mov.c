047e88c4063a12a1e8f2abf33ecf2dc57e8e86f2
tracer-ics2017 <tracer@njuics.org>
2021-03-24 00:10:31 +0800

    >  compile 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  00:10:31 up  1:14,  3 users,  load average: 0.75, 0.63, 0.62 5b502aa5dd5882fa842e7abe2fc1f4c399344a52

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -96,7 +96,7 @@ make_EHelper(movsx) {
   printf("src_width=%d\ndest_width=%d\nval=%08X\n",id_src->width,id_dest->width,id_src->val);
   printf("t21=%08X\n",t2);
   rtl_sext(&t2, &id_src->val, id_src->width);
-  id_dest->width=id_src->width;
+  //id_dest->width=id_src->width;
   printf("t2=%08X\n",t2);
   operand_write(id_dest, &t2);
   print_asm_template2(movsx);
