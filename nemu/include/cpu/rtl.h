932d6e666a15c7356f3c6b41352f69a65a1048b6
tracer-ics2017 <tracer@njuics.org>
2021-03-24 17:11:16 +0800

    >  compile 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  17:11:16 up 17 min,  3 users,  load average: 0.53, 0.64, 0.41 f24af5cd52003f32b56641d1a0260da75677f6e5

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -197,6 +197,7 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   rtlreg_t temp;
   rtl_msb(&temp,src1,width);
+  temp &= 1;
   printf("temp=%08X\n",temp);
   if(temp==0)
   {
