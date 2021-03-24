9fdd218334ae2bc5b77d83307adc2a765b7321a8
tracer-ics2017 <tracer@njuics.org>
2021-03-24 22:22:16 +0800

    >  compile 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  22:22:16 up  5:28,  3 users,  load average: 0.84, 0.81, 0.80 7635fa1e2478b6a49022abc4c13c8823a18cc90f

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -318,10 +318,10 @@ make_EHelper(imul1) {
 make_EHelper(imul2) {
   rtl_sext(&id_src->val, &id_src->val, id_src->width);
   rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
-  printf("dest=%08X\nsrc=%08X\n",id_dest->val,id_src->val);
+  //printf("dest=%08X\nsrc=%08X\n",id_dest->val,id_src->val);
   rtl_imul(&t0, &t1, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t1);
-  printf("t0=%08X\nt1=%08X\n",t0,t1);
+  //printf("t0=%08X\nt1=%08X\n",t0,t1);
   /*
     Clearing CF and OF when:
     r16,r/m16   Result exactly fits within r16
