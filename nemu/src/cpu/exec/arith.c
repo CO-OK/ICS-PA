3dc246c57c34d64ba07bd0afb02ea3d724cbf3a2
tracer-ics2017 <tracer@njuics.org>
2021-03-25 09:59:53 +0800

    >  compile 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  09:59:53 up  1:14,  3 users,  load average: 1.12, 0.94, 0.86 947ab1541e90e279b92118d1d0f1225a2acc7d98

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -174,7 +174,7 @@ make_EHelper(inc) {
 
 make_EHelper(dec) {
   //TODO();
-  printf("val=%08X\n",id_dest->val);
+  //printf("val=%08X\n",id_dest->val);
   rtl_subi(&t0,&id_dest->val,1);
   rtl_sr(id_dest->reg,id_dest->width,&t0);
   rtl_update_ZFSF(&t0,id_dest->width);  
