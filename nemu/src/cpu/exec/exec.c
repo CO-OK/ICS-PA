1578191b367d5cc593bc307780028742f544d412
tracer-ics2017 <tracer@njuics.org>
2021-03-25 09:27:58 +0800

    >  compile 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  09:27:58 up 42 min,  3 users,  load average: 0.45, 0.62, 0.66 6ac776b7c21d3e8317cb1858bbcd6a98c786898

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -99,7 +99,7 @@ opcode_entry opcode_table [512] = {
   /* 0x5c */	IDEX(r,pop_reg), IDEX(r,pop_reg), IDEX(r,pop_reg), IDEX(r,pop_reg),
   /* 0x60 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x64 */	EMPTY, EMPTY, EX(operand_size), EMPTY,
-  /* 0x68 */	EMPTY, EMPTY, IDEXW(I,push_reg,1), EMPTY,//0x6a push Ib,就写成push_reg了懒得改
+  /* 0x68 */	IDEXW(I,push_reg,1), EMPTY, IDEXW(I,push_reg,1), EMPTY,//0x6a push Ib,就写成push_reg了懒得改//0x68 push Ib
   /* 0x6c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x70 */	IDEXW(J,jcc,1), IDEXW(J,jcc,1), IDEXW(J,jcc,1), IDEXW(J,jcc,1),
   /* 0x74 */	IDEXW(J,jcc,1), IDEXW(J,jcc,1), IDEXW(J,jcc,1), IDEXW(J,jcc,1),//0x74 je/jz  //0x75 jnz
