693e197088fa9138ba07efb70e105f654d0c34c8
tracer-ics2017 <tracer@njuics.org>
2021-03-25 23:14:31 +0800

    >  compile 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  23:14:31 up  3:53,  3 users,  load average: 0.29, 0.52, 0.54 50d18e7392d7c93d844594a2ab5113799bfcf121

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -46,7 +46,7 @@ void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
   //TODO();
-  t0=pio_read(id_src->val,id_src->width);
+  t0=pio_read(id_src->val,id_src->width*8);
   operand_write(id_dest,&t0);
   print_asm_template2(in);
 
@@ -57,7 +57,7 @@ make_EHelper(in) {
 
 make_EHelper(out) {
   //TODO();
-  pio_write(id_dest->val,id_dest->width,id_src->val);
+  pio_write(id_dest->val,id_src->width*8,id_src->val);
   print_asm_template2(out);
 
 #ifdef DIFF_TEST
