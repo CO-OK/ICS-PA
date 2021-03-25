28efe2a305baf28a339e0126f6cfb8f2b8236192
tracer-ics2017 <tracer@njuics.org>
2021-03-25 10:20:09 +0800

    >  compile 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  10:20:09 up  1:34,  3 users,  load average: 0.43, 0.41, 0.60 c0461eebe994e9a9be6bfd8d35ff8d4324dc3a4

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -387,7 +387,7 @@ make_EHelper(imul1) {
 make_EHelper(imul2) {
   rtl_sext(&id_src->val, &id_src->val, id_src->width);
   rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
-  printf("dest=%08X\nsrc=%08X\n",id_dest->val,id_src->val);
+  printf("dest=%08X\nsrc=%08Xwidth=%d\n\n",id_dest->val,id_src->val,id_dest->width);
   t0=0;
   rtl_imul(&t0, &t1, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t1);
