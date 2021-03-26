c569a766e5a67c051352eb513da7a6026b58dbea
tracer-ics2017 <tracer@njuics.org>
2021-03-26 20:47:51 +0800

    >  run 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  20:47:50 up  1:41,  4 users,  load average: 0.30, 0.62, 1.04 f26d6f30a43cc5c5d907104aedaa8f612272b10c

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -37,10 +37,9 @@ void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
   for (i = 0; i < _screen.width * _screen.height; i++) {
     fb[i] = i;
   }*/
-  int cp_bytes = sizeof(uint32_t) * min(w, _screen.width - x);
-  for (int j = 0; j < h && y + j < _screen.height; j ++) {
-    memcpy(&fb[(y + j) * _screen.width + x], pixels, cp_bytes);
-    pixels += w;
+  for(int i=0;i< w*h;i++)
+  {
+    fb[x*y+i]=*pixels;
   }
 }
 
