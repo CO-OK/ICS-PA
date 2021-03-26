d7d272968611384fda74ca9382a957ddc5ba90a0
tracer-ics2017 <tracer@njuics.org>
2021-03-26 21:01:01 +0800

    >  run 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  21:01:01 up  1:54,  4 users,  load average: 1.30, 0.97, 0.93 49f390baac010e841939d344f2b8f909588c71cd

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -42,8 +42,9 @@ void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
     for(int j=0;j<h;j++)
     {
       fb[_screen.width*(x+i)+y+j]=*pixels;
-      pixels+=w;
+      pixels+=h;
     }
+    pixels+=w;
   }
 
 }
