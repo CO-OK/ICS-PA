06bef0d43854980ac36c0d72b973e4bb32943a36
tracer-ics2017 <tracer@njuics.org>
2021-03-26 19:29:08 +0800

    >  run 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  19:29:08 up 23 min,  3 users,  load average: 0.74, 0.72, 0.63 2273b16cddf996fdc286e61ea7bd409efaa4e1a7

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -10,7 +10,7 @@ void _ioe_init() {
 
 unsigned long _uptime() {
   unsigned long now_time=inl(RTC_PORT);
-  return now_time-boot_time;
+  return (now_time-boot_time)/1000;
 }
 
 uint32_t* const fb = (uint32_t *)0x40000;
