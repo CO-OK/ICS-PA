06099a8c65cdf69c983ac022211e860d62be1a6c
tracer-ics2017 <tracer@njuics.org>
2021-03-27 11:06:13 +0800

    >  run 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  11:06:13 up 20 min,  3 users,  load average: 0.76, 0.66, 0.39 f402008842a26ac6dab54692cdb7696ea2934596

--- nexus-am/tests/timetest/main.c
+++ nexus-am/tests/timetest/main.c
@@ -4,13 +4,17 @@
 int main(){
   _ioe_init();
   int sec = 1;
-  while (1) {
+  while (1) 
+  {
     while(_uptime() < 1000 * sec) ;
-    if (sec == 1) {
+    if (sec == 1) 
+    {
       printf("%d second.\n", sec);
-    } else {
+    } else 
+    {
       printf("%d seconds.\n", sec);
     }
+    printf("uptime=%d\n",_uptime());
     sec ++;
   }
   return 0;
