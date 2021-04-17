34cd33cd0f173eb8e673bb28d3deefbfc0068c35
tracer-ics2017 <tracer@njuics.org>
2021-04-17 20:53:02 +0800

    >  compile 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  20:53:02 up  2:13,  4 users,  load average: 0.85, 0.55, 0.56 3cc85b2c5b3755a8f655cb4d786efa2dc340941

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -21,14 +21,14 @@ make_EHelper(lidt) {
   }
   else
   {
-    printf("testaddr=%08X\n",vaddr_read(id_dest->addr,4));
+    //printf("testaddr=%08X\n",vaddr_read(id_dest->addr,4));
     uint32_t low_four_byte=vaddr_read(id_dest->addr,4);//读出来的6byte数据中的低4byte
     uint16_t high_two_byte=vaddr_read(id_dest->addr+4,2);//读出来的6byte数据中的高2byte
-    printf("high=%08X,low=%08X\n",high_two_byte,low_four_byte);
+    //printf("high=%08X,low=%08X\n",high_two_byte,low_four_byte);
     uint32_t temp_base=(low_four_byte<<8)>>8;
-    printf("tempbase=%08X\n",temp_base);
+    //printf("tempbase=%08X\n",temp_base);
     uint16_t temp_limt=low_four_byte>>24;
-    printf("templimt=%08X\n",temp_limt);
+    //printf("templimt=%08X\n",temp_limt);
     cpu.idtr_base = temp_base + (((uint32_t)(high_two_byte>>8))<<24);
     cpu.idtr_limit=temp_limt+(high_two_byte<<8);
     //printf("limt=%08X\n",limt);
