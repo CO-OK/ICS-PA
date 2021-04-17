eecac3078ec0c4405f57d1f26b35fc8b779983aa
tracer-ics2017 <tracer@njuics.org>
2021-04-17 19:56:48 +0800

    >  run 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  19:56:48 up  1:16,  4 users,  load average: 0.73, 0.63, 0.61 4d8ffaf4a2919f98e012ddbfd15eff1e78879765

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,6 +3,11 @@
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  //TODO();
+  /*
+    目前的 loader 只需要做一件事情:将 ramdisk 中从 0 开始的所有内容放置在 0x4000000,并把这个地
+    址作为程序的入口返回即可
+  */
+  ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
