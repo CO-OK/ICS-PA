ec63110fe8c038e7df4767a6fbb00de9c469cc58
tracer-ics2017 <tracer@njuics.org>
2021-04-17 20:02:50 +0800

    >  compile 1410576 v Linux v-pc 5.4.105-1-MANJARO #1 SMP PREEMPT Thu Mar 11 18:27:58 UTC 2021 x86_64 GNU/Linux  20:02:50 up  1:22,  4 users,  load average: 1.25, 1.45, 1.04 cdb37ac76b823eaedcd4860ba189e42d1ebf8e32

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -67,7 +67,7 @@ make_group(gp5,
 
   /* 0x0f 0x01*/
 make_group(gp7,
-    EMPTY, EMPTY, EMPTY, IDEXW(I,lidt,0),
+    EMPTY, EMPTY, EMPTY, IDEXW(gp7_E,lidt,0),
     EMPTY, EMPTY, EMPTY, EMPTY)
 
 /* TODO: Add more instructions!!! */
