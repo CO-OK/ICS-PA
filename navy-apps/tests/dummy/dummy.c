5ab49955a7e49ee1fae1a42b8ba472bf9b4b9ed9
tracer-ics2017 <tracer@njuics.org>
2021-04-22 09:19:54 +0800

    >  run 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  09:19:54 up 30 min,  4 users,  load average: 0.59, 0.55, 0.46 a3ee5c1d6c3ad3c57a6c164186149e91c8481a29

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -5,6 +5,7 @@ extern int _syscall_(int, intptr_t, intptr_t, intptr_t);
 
 int main() {
   // this system call will trap into OS but do nothing
-  int r = _syscall_(SYS_none, 0, 0, 0);
-  return (r == 1 ? 0 : 1);
+  //int r = _syscall_(SYS_none, 0, 0, 0);
+  //return (r == 1 ? 0 : 1);
+  return 0;
 }
