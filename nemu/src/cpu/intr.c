31352ea4dee35008bd89cd54f6bb2ddcdebe879c
tracer-ics2017 <tracer@njuics.org>
2021-04-22 23:36:08 +0800

    >  compile 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  23:36:08 up  2:12,  4 users,  load average: 0.88, 0.63, 0.57 a735f45ee17f9f7d66497ceb13e6e9ff4bd8cb4a

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -14,13 +14,13 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
     将门描述符中的 offset 域组合成目标地址
     跳转到目标地址
   */
-  printf("size=%d\n",sizeof(GateDesc));
+  printf("size=%ld\n",sizeof(GateDesc));
   printf("hit raise_intr\n");
   rtl_push(&cpu.EFLAGS_,4);
   rtl_push(&cpu.cs,4);
   rtl_push(&ret_addr,4);
   printf("eip==%08X\n",cpu.eip);
-  vaddr_t gate_enrty= cpu.idtr_base + sizeof(GateDesc)*NO;
+  vaddr_t gate_enrty= cpu.idtr_base + NO;
   printf("gate entry=%08X\n",gate_enrty);
   uint32_t off15to0 = vaddr_read(gate_enrty,4);
   uint32_t off16to32 = vaddr_read(gate_enrty+32,4);
