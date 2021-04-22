2eb1c67a692803124b70caf7cf046e0a2c207dcc
tracer-ics2017 <tracer@njuics.org>
2021-04-22 23:01:13 +0800

    >  compile 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  23:01:13 up  1:37,  4 users,  load average: 0.15, 0.22, 0.45 44803afde98e26b85837b43219e14e91688bfd72

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -17,6 +17,7 @@ make_EHelper(lidt) {
     cpu.idtr_base=vaddr_read(id_dest->addr+2,4);
     printf("limt=%08X\n",cpu.idtr_limit);
     printf("base=%08X\n",cpu.idtr_base);
+
   }
   printf("hit lidt\n");
   printf("destaddr=%08X\n",id_dest->addr);
