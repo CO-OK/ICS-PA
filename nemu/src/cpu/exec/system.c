cdf74d8dcadf06f41bff7e47c07253e5fc5da441
tracer-ics2017 <tracer@njuics.org>
2021-04-22 23:18:40 +0800

    >  compile 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  23:18:40 up  1:54,  4 users,  load average: 0.62, 0.44, 0.37 c5993295a7fd46952ad3ebafaa971a316c99baea

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -13,13 +13,13 @@ make_EHelper(lidt) {
   }
   else
   {
-    /*cpu.idtr_limit=vaddr_read(id_dest->addr,4);
-    cpu.idtr_base=vaddr_read(id_dest->addr+2,4);*/
-    uint32_t low4=vaddr_read(id_dest->addr,4);
+    cpu.idtr_limit=vaddr_read(id_dest->addr,4);
+    cpu.idtr_base=vaddr_read(id_dest->addr+2,4);
+    /*uint32_t low4=vaddr_read(id_dest->addr,4);
     uint32_t high4=vaddr_read(id_dest->addr+2,4);
     printf("high=%08X,low=%08X\n",high4&0xff000000,low4&0x00ffffff);
     cpu.idtr_limit = (high4>>8)&0x0000ffff;
-    cpu.idtr_base=(low4&0x00ffffff)+(high4&0xff000000);
+    cpu.idtr_base=(low4&0x00ffffff)+(high4&0xff000000);*/
     printf("limt=%08X\n",cpu.idtr_limit);
     printf("base=%08X\n",cpu.idtr_base);
 
