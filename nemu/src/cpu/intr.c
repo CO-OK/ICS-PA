4233003dbb1228cc84e1e84feaa2af19606391c4
tracer-ics2017 <tracer@njuics.org>
2021-04-22 21:47:23 +0800

    >  compile 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  21:47:23 up 23 min,  3 users,  load average: 0.85, 0.66, 0.38 17b21b489b7d60588274ba1882d7544c3842f8c6

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -18,6 +18,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push(&cpu.EFLAGS_,4);
   rtl_push(&cpu.cs,4);
   rtl_push(&cpu.eip,4);
+  printf("eip==%08X\n",cpu.eip);
   vaddr_t gate_enrty= cpu.idtr_base + sizeof(GateDesc)*NO;
   uint32_t off15to0 = vaddr_read(gate_enrty,4);
   uint32_t off16to32 = vaddr_read(gate_enrty+32,4);
