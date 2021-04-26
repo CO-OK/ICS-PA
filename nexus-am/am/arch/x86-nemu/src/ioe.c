4bbabcdba7731bcd3d62d37772fa1cb23d9b378a
tracer-ics2017 <tracer@njuics.org>
2021-04-26 20:04:04 +0800

    >  run 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  20:04:04 up 27 min,  4 users,  load average: 0.77, 0.71, 0.60 dc3712087977cf094f862a643326791744477eeb

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -56,7 +56,7 @@ int _read_key() {
   /*
     i8042 初始化时会注册 0x60 处的端口作为数据寄存器,注册 0x64 处的端口作为状态寄存器
   */
-  uint32_t is_key_event = inl(0x64);
+  uint32_t is_key_event = inb(0x64);
   uint32_t data = inl(0x60);
   if(is_key_event!=0)
   {
