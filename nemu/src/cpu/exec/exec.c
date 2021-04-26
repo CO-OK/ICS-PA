0d0cfd91fe75b37731ccca4a4a85502ebe8e50de
tracer-ics2017 <tracer@njuics.org>
2021-04-26 20:54:09 +0800

    >  compile 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  20:54:09 up  1:18,  5 users,  load average: 1.09, 0.93, 0.73 e0cfe8c3880091465baef13624027bc1c880de19

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -81,7 +81,7 @@ opcode_entry opcode_table [512] = {
   /* 0x14 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x18 */	EMPTY, IDEXW(G2E,sbb,0), EMPTY, IDEXW(E2G,sbb,0),//0x1b  sbb Gv,Ev 
   /* 0x1c */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x20 */	IDEXW(E2G,and,1), IDEXW(E2G,and,0), IDEXW(E2G,and,1), IDEXW(E2G,and,0),// 20---->25 and
+  /* 0x20 */	IDEXW(E2G,and,1), IDEXW(G2E,and,0), IDEXW(E2G,and,1), IDEXW(E2G,and,0),// 20---->25 and
   /* 0x24 */	IDEXW(I2a,and,1), IDEXW(I2a,and,0), EMPTY, EMPTY,
   /* 0x28 */	EMPTY, IDEXW(G2E,sub,0), EMPTY, IDEXW(E2G,sub,0),//29 sub EV,Gv //0x2b sub  Gv,Ev
   /* 0x2c */	EMPTY, IDEXW(I2a,sub,0), EMPTY, EMPTY,//2d sub EaX,Iv
