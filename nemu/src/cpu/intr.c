5358ba1dcdcfb73872e239417c03d4b6dd852056
tracer-ics2017 <tracer@njuics.org>
2021-04-28 21:13:54 +0800

    >  compile 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  21:13:54 up  4:22,  3 users,  load average: 1.02, 0.95, 0.81 2c59732a22a789505782126188d55fffa6dcdbd4

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -14,22 +14,22 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
     将门描述符中的 offset 域组合成目标地址
     跳转到目标地址
   */
-  printf("size=%ld\n",sizeof(GateDesc));
-  printf("hit raise_intr\n");
+  //printf("size=%ld\n",sizeof(GateDesc));
+  //printf("hit raise_intr\n");
   rtl_push(&cpu.EFLAGS_,4);
   rtl_push(&cpu.cs,4);
   rtl_push(&ret_addr,4);
-  printf("eip==%08X\n",cpu.eip);
+  //printf("eip==%08X\n",cpu.eip);
   vaddr_t gate_enrty= cpu.idtr_base + sizeof(GateDesc)*NO;
-  printf("gate entry=%08X\n",gate_enrty);
+  //printf("gate entry=%08X\n",gate_enrty);
   uint32_t off15to0 = vaddr_read(gate_enrty,2);
   uint32_t off16to32 = vaddr_read(gate_enrty+6,8);
-  printf("15t0=%08X 16t32=%08X\n",off15to0,off16to32);
-  printf("temp=%08X\n",off16to32>>16);
+  //printf("15t0=%08X 16t32=%08X\n",off15to0,off16to32);
+  //printf("temp=%08X\n",off16to32>>16);
   vaddr_t final=(off16to32<<16)+(0x0000ffff&off15to0);
   decoding.seq_eip=final;
-  printf("eip=%08X\n",final);
-  printf("1\n");
+  //printf("eip=%08X\n",final);
+  //printf("1\n");
 }
 
 void dev_raise_intr() {
