4470012ba03b464cadefe816d919bd275108d2e3
tracer-ics2017 <tracer@njuics.org>
2021-04-29 11:11:11 +0800

    >  compile 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  11:11:11 up 41 min,  5 users,  load average: 0.74, 0.66, 0.64 4cf5018012126e9c56e543a1287cd11925ecbc0d

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -62,6 +62,7 @@ make_EHelper(int) {
 make_EHelper(iret) {
   //TODO();
   rtl_pop(&decoding.jmp_eip);
+  printf("$eip=%08X\n",decoding.jmp_eip);
   rtl_pop(&cpu.cs);
   rtl_pop(&cpu.EFLAGS_);
   decoding.is_jmp=1;
