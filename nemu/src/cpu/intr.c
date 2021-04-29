7017db02c2b13aa8988941d48cc334a7dfda164e
tracer-ics2017 <tracer@njuics.org>
2021-04-29 16:20:26 +0800

    >  compile 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  16:20:26 up 47 min,  3 users,  load average: 1.12, 1.12, 0.80 ed70a8ac483b2e7326e7e523bbf9e41d8d2f56ac

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -32,6 +32,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   //decoding.seq_eip=final;
   decoding.is_jmp=1;
   decoding.jmp_eip=final;
+  printf("cpu.jmpeip=%08X\n",decoding.jmp_eip);
   //printf("raise esp %08X\n",cpu.esp);
   //printf("jmp eip=%08X\n",final);
 }
