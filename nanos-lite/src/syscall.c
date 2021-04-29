0f8e8eee11da47b494c24e90657800f1de4f45f8
tracer-ics2017 <tracer@njuics.org>
2021-04-29 17:09:37 +0800

    >  run 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  17:09:37 up  1:36,  3 users,  load average: 0.75, 0.71, 0.65 bde94893c3f16cf29e0fde22e16c6718e7fc46c

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,8 +4,6 @@
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
-  for(int i=0;i<4;i++)
-    printf("a%d=%08X\n",i,a[i]);
   //printf("call num=%d\n",a[0]);
   switch (a[0]) {
     case 0:{
