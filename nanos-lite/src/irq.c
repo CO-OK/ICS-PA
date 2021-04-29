2f7883f6da1189642255db99eb884506fed99917
tracer-ics2017 <tracer@njuics.org>
2021-04-29 16:58:41 +0800

    >  run 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  16:58:41 up  1:25,  3 users,  load average: 0.56, 0.69, 0.64 d38aa7674ee308da33c132355d92c8f59fc2d7fc

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,6 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
     //
     case _EVENT_SYSCALL:{
       do_syscall(r);
-      printf("hit\n");
       break;
     }
     default: panic("Unhandled event ID = %d", e.event);
