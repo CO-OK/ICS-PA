51335059e95d2ab3e0a31a525f518d73e7f175d7
tracer-ics2017 <tracer@njuics.org>
2021-04-29 11:12:56 +0800

    >  run 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  11:12:56 up 43 min,  5 users,  load average: 0.82, 0.73, 0.66 20b42bf9547b0647cc83fc3dfc53452c0720984

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,7 +8,7 @@ _RegSet* do_syscall(_RegSet *r) {
   switch (a[0]) {
     case 0:{
       sys_none();
-      //break;
+      break;
     }
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
