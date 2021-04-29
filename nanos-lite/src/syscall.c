2a1db333a1a044539dba8d0432eeb46500ca9607
tracer-ics2017 <tracer@njuics.org>
2021-04-29 17:05:48 +0800

    >  run 1410576 v Linux v-pc 5.4.112-1-MANJARO #1 SMP PREEMPT Wed Apr 14 07:47:45 UTC 2021 x86_64 GNU/Linux  17:05:48 up  1:32,  3 users,  load average: 1.17, 0.69, 0.64 61548fd65b10baafbaf614fa084b982561fe6f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,7 +7,7 @@ _RegSet* do_syscall(_RegSet *r) {
   //printf("call num=%d\n",a[0]);
   switch (a[0]) {
     case 0:{
-      a[6]=sys_none();
+      a[7]=sys_none();
       break;
     }
     case 4:{
