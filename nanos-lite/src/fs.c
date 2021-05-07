f60e6f675b0d03a5a1f867a43182eba1269ee41f
tracer-ics2017 <tracer@njuics.org>
2021-05-07 23:48:26 +0800

    >  run 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  23:48:26 up  4:13,  5 users,  load average: 0.78, 0.77, 0.80 820304c7afb708141c9f800161bdadc8660a9908

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -38,7 +38,7 @@ int fs_read(int fd, void *buf, size_t count)
 {
   if (file_table[fd].open_offset + count > fs_filesz(fd))
 		count = file_table[fd].size - file_table[fd].open_offset;
-  ramdisk_read(buf, file_table[fd].disk_offset , count);
+  ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, count);
   file_table[fd].open_offset+=count;
 }
 
