f17a4bc02e258f72f4b0f82d6e631b56cc323198
tracer-ics2017 <tracer@njuics.org>
2021-05-07 21:24:38 +0800

    >  run 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  21:24:38 up  1:49,  5 users,  load average: 0.48, 0.76, 0.93 458d5249aa45130ad166454fa47fbe5f31b455e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -55,10 +55,10 @@ int sys_write(int fd, char *buf, size_t count)
   //printf("fd=%d\ncount=%d\n",fd,count);
   if(fd==1||fd==2)
   {
-    for(int i=0;i<count;i++)
+    /*for(int i=0;i<count;i++)
     {
       _putc(buf[i]);
-    }
+    }*/
     return count;
   }
   return -1;
