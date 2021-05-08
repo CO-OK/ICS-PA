cb60321e98704803de7c61d5646c3435e3af9604
tracer-ics2017 <tracer@njuics.org>
2021-05-08 10:01:16 +0800

    >  run 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  10:01:16 up 11 min,  3 users,  load average: 1.15, 0.68, 0.34 7d74664ec3e6a5fed395a32a7b748c57e3a12476

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -31,7 +31,7 @@ int fs_open(char*path)
   {
     if(strcmp(file_table[i].name,path)==0)
       return i;
-    printf("str1=%s\n",file_table[i].name);
+    //printf("str1=%s\n",file_table[i].name);
   }
   assert(0);
   return -1;
