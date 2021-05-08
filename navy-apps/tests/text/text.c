285cabea0652c4258b2c561bd319be01b8f4c98a
tracer-ics2017 <tracer@njuics.org>
2021-05-08 15:15:49 +0800

    >  run 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  15:15:49 up  1:35,  3 users,  load average: 0.94, 0.59, 0.59 63a0897b1bf5f00b2a96388ebca74056ea56a53

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -20,12 +20,12 @@ int main() {
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
-  /*printf("3\n");
+  printf("3\n");
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-  printf("4\n");
+  /*printf("4\n");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
