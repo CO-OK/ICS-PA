b2e7bfc8c35cf14badff03755c42e4741d863aa6
tracer-ics2017 <tracer@njuics.org>
2021-05-08 13:57:53 +0800

    >  run 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  13:57:53 up 17 min,  3 users,  load average: 0.53, 0.51, 0.39 daf81912fffbd9b5c71137ac683b9038e9147c74

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -23,7 +23,7 @@ _RegSet* do_syscall(_RegSet *r) {
       printf("arg4=%08X\n",SYSCALL_ARG4(r));*/
       //return 0;
       //printf("hit write\n");
-      SYSCALL_ARG1(r)=sys_write(SYSCALL_ARG4(r),SYSCALL_ARG2(r),SYSCALL_ARG3(r));
+      SYSCALL_ARG1(r)=fs_write(SYSCALL_ARG4(r),SYSCALL_ARG2(r),SYSCALL_ARG3(r));
       break;
     }
     case SYS_open:{
