4e9cb050919a373d1fcd33f18cbba432465cd675
tracer-ics2017 <tracer@njuics.org>
2021-05-10 22:56:36 +0800

    >  compile 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  22:56:36 up  3:19,  4 users,  load average: 0.75, 0.99, 1.02 635af2deb12b1351aee2feeb9b3a2e84e6ec93bd

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -79,61 +79,27 @@ make_EHelper(cltd) {
 
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
-    TODO();
-  }
-  else {
-    TODO();
-  }
-
-  print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
+		rtl_shli(&reg_l(R_EAX), &reg_l(R_EAX), 24);
+		rtl_sari(&reg_l(R_EAX), &reg_l(R_EAX), 8);
+	  rtl_shri(&reg_l(R_EAX), &reg_l(R_EAX), 16);
+	}
+	else {
+		rtl_sext(&reg_l(R_EAX), &reg_l(R_EAX), 2);
+	} 
+	print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
 }
 
 make_EHelper(movsx) {
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
-  //printf("src_width=%d\ndest_width=%d\nval=%08X\n",id_src->width,id_dest->width,id_src->val);
-  //printf("t21=%08X\n",t2);
-  uint32_t t = decoding.opcode & 0x000000ff;
-  if(t==0xbe)
-  {
-    //Move byte to dword, sign-extend
-    rtl_sext(&t2, &id_src->val, 1);
-  }
-  else if(t==0xbf)
-  {
-    //Move word to dword, sign-extend
-    rtl_sext(&t2, &id_src->val, 2);
-  }
-  
-  //printf("t22=%08X\n",t2);
-  //id_dest->width=id_src->width;
-  operand_write(id_dest, &t2);
-  print_asm_template2(movsx);
+	rtl_sext(&t0, &id_src->val, id_src->width);
+	operand_write(id_dest, &t0);
+	print_asm_template2(movsx);
 }
 
 make_EHelper(movzx) {
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
-  //printf("op=%X\n",decoding.opcode);
-  /*
-    这里应该根据操作码来决定操作数的宽度？
-  */
-  uint32_t t = decoding.opcode & 0x000000ff;
-  //printf("op=%X\n",t);
-  if(t==0xb6)
-  {
-    //Move byte to dword, zero-extend
-    id_src->val &= 0x000000ff;
-  }
-  else if(t==0xb7)
-  {
-    //Move word to dword, zero-extend
-    id_src->val &= 0x0000ffff;
-  }
-
-  //printf("dest_width=%d\n",id_dest->width);
-  //printf("src_width=%d\n",id_src->width);
-  //printf("val=%08X\n",id_src->val);
-  operand_write(id_dest, &id_src->val);
-  print_asm_template2(movzx);
+	operand_write(id_dest, &id_src->val);
+	print_asm_template2(movzx);
 }
 
 make_EHelper(lea) {
