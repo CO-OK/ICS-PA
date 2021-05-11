d7e7c01d2644ee67edb37192292719efe268672b
tracer-ics2017 <tracer@njuics.org>
2021-05-11 16:04:04 +0800

    >  compile 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  16:04:04 up 59 min,  4 users,  load average: 0.68, 0.95, 1.49 ec32315aebff3029bc5125d33a930c1da5fff99b

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -17,8 +17,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   //printf("size=%ld\n",sizeof(GateDesc));
   //printf("hit raise_intr\n");
   //printf("in raise eip=%08X\n",cpu.eip);
-
-  /*Log("hit raise_intr with NO=%X",NO);
+  Log("hit raise_intr with NO=%X",NO);
   rtl_push(&cpu.EFLAGS_,4);
   rtl_push(&cpu.cs,4);
   rtl_push(&ret_addr,4);
@@ -34,34 +33,10 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   //decoding.seq_eip=final;
   decoding.is_jmp=1;
   decoding.jmp_eip=final;
+  cpu.eip=final;
   Log("in raise_intr eax=%08X,ebx=%08X,ecx=%08X,edx=%08X",cpu.eax,cpu.ebx,cpu.ecx,cpu.edx);
   Log("in raise_intr esp=%08X ebp=%08X",cpu.esp,cpu.ebx);
-  Log("finish raise_intr with NO=%X target addr = %08X",NO,final);*/
-
-  union {
-		GateDesc gd;
-		struct { uint32_t lo, hi; };
-	} item;
-	vaddr_t addr;
-	
-	addr = 8 * NO + cpu.idtr_base;
-	item.lo = vaddr_read(addr, 4);
-	item.hi = vaddr_read(addr + 4, 4);
-
-	
-	t1 = (item.gd.offset_15_0 & 0xFFFF)
-	| ((item.gd.offset_31_16 & 0xFFFF) << 16);
-  
-	//rtl_j(t1);
-	cpu.eip=t1;
-  decoding.is_jmp=1;
-	// 保存eflags cs 返回地址
-	rtl_push(&cpu.EFLAGS_,4);
-	t0 = cpu.cs;
-	rtl_push(&t0,4);
-	rtl_push(&ret_addr,4);
-	
-	//cpu.eflags.IF = 0;
+  Log("finish raise_intr with NO=%X target addr = %08X",NO,final);
 
   //printf("cpu.jmpeip=%08X\n",decoding.jmp_eip);
   //printf("raise esp %08X\n",cpu.esp);
