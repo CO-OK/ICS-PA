f238178a1cd6ff2a176625e2ff725b7a47775f1b
tracer-ics2017 <tracer@njuics.org>
2021-05-13 09:05:20 +0800

    >  compile 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  09:05:20 up 18 min,  3 users,  load average: 0.57, 0.88, 0.71 1b99f012d92c0ec59a15a35dfc2a6bf3222eaf31

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -33,7 +33,7 @@ int fs_open(char*path,int flags,int mode)
   {
     if(strcmp(file_table[i].name,path)==0)
     {
-      //Log("open %s,fd=%d\n",path,i);
+      Log("open %s,fd=%d\n",path,i);
       return i;
       //printf("out\n");
     }
@@ -44,7 +44,7 @@ int fs_open(char*path,int flags,int mode)
 }
 ssize_t fs_read(int fd, void *buf, size_t count)
 {
-  //Log("read %s fileSize=%d count=%d,open_offset=%d\n",file_table[fd].name,file_table[fd].size,count,file_table[fd].open_offset,((uint32_t*)(buf)));
+  Log("read %s fileSize=%d count=%d,open_offset=%d\n",file_table[fd].name,file_table[fd].size,count,file_table[fd].open_offset,((uint32_t*)(buf)));
   if(file_table[fd].open_offset >= fs_filesz(fd))
 		return 0;
   if (file_table[fd].open_offset + count > fs_filesz(fd))
@@ -53,7 +53,7 @@ ssize_t fs_read(int fd, void *buf, size_t count)
   ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, count);
 
   file_table[fd].open_offset+=count;
-  //Log("fs_read return ,open_offset=%d",file_table[fd].open_offset);
+  Log("fs_read return ,open_offset=%d",file_table[fd].open_offset);
   return count;
 }
 
@@ -65,13 +65,13 @@ ssize_t fs_filesz(int fd)
 int fs_close(int fd)
 {
   assert(fd>=0&&fd<NR_FILES);
-  //Log("close %s\n",file_table[fd].name);
+  Log("close %s\n",file_table[fd].name);
   //file_table[fd].open_offset=0;
   return 0;
 }
 off_t fs_lseek(int fd, off_t offset, int whence)
 {
-  //Log("lseek %d offset=%d,whence=%d,filesize=%d\n",fd,offset,whence,file_table[fd].size);
+  Log("lseek %d offset=%d,whence=%d,filesize=%d\n",fd,offset,whence,file_table[fd].size);
   if(whence==SEEK_CUR)
   {
     if ((offset + file_table[fd].open_offset >= 0) && (offset + file_table[fd].open_offset <= fs_filesz(fd))) 
@@ -109,7 +109,7 @@ off_t fs_lseek(int fd, off_t offset, int whence)
 
 ssize_t fs_write(int fd, void *buf, size_t count)
 {
-  //Log("write %d name=%s count=%d\n",fd,file_table[fd].name,count);
+  Log("write %d name=%s count=%d\n",fd,file_table[fd].name,count);
   if (file_table[fd].open_offset + count > fs_filesz(fd))
 		count = file_table[fd].size - file_table[fd].open_offset;
   ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, count);
