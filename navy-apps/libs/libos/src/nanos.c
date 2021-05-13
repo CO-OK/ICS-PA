e5fa6a20f821de520c349d86f9c6222ac88f233f
tracer-ics2017 <tracer@njuics.org>
2021-05-13 09:43:12 +0800

    >  compile 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  09:43:12 up 56 min,  3 users,  load average: 1.55, 1.16, 1.05 4e0a7747e0c2d135d0c8ea09a4bc2bac3f307e0d

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -36,21 +36,17 @@ int _write(int fd, void *buf, size_t count){
 }
 
 void *_sbrk(intptr_t increment){
-  
+  Assert(0);
 	intptr_t prev = program_break;
-	if (_syscall_(SYS_brk, prev + increment, 0, 0) == 0) {
-		//panic("222");
-    
+	if (_syscall_(SYS_brk, prev + increment, 0, 0) == 0) { 
 		program_break += increment;	
 		return (void*)prev;
 	}
 	else 
   {
-    //panic("222");
     assert(0);
 		return (void *)-1;
 	}
-  //panic("222");
 }
 
 int _read(int fd, void *buf, size_t count) {
