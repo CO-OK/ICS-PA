fa950c27e96bd4bbc69e5e27af8371bf65c1a46e
tracer-ics2017 <tracer@njuics.org>
2021-05-13 09:52:52 +0800

    >  run 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  09:52:52 up  1:06,  4 users,  load average: 0.26, 0.74, 0.91 67c875c17382bbc6570626a942a37281e628abc4

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -5,12 +5,12 @@
 #include <assert.h>
 #include <time.h>
 #include "syscall.h"
-
+extern char _end;
+intptr_t program_break = (intptr_t)&_end;
 
 // TODO: discuss with syscall interface
 #ifndef __ISA_NATIVE__
-extern char _end;
-intptr_t program_break = (intptr_t)&_end;
+
 // FIXME: this is temporary
 
 int _syscall_(int type, uintptr_t a0, uintptr_t a1, uintptr_t a2){
@@ -36,7 +36,6 @@ int _write(int fd, void *buf, size_t count){
 }
 
 void *_sbrk(intptr_t increment){
-  //Assert(0);
 	intptr_t prev = program_break;
 	if (_syscall_(SYS_brk, prev + increment, 0, 0) == 0) 
   { 
