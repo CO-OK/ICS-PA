e04646c41af78aa7998c1e46b7fa0f59e8e1925a
tracer-ics2017 <tracer@njuics.org>
2021-05-15 20:36:33 +0800

    >  run 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  20:36:33 up  5:25,  4 users,  load average: 1.41, 1.16, 1.23 bb9491e6f774f06a5d6a1ad3e6ad8aa758dc4294

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -16,7 +16,7 @@ static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
   strncpy(buf, dispinfo + offset, len);
-	return len;
+
 }
 extern int screen_width();
 extern int screen_height();
