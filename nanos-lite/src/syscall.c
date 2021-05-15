66d5c81746a0864c006384e449d3bfeed8e0b3c1
tracer-ics2017 <tracer@njuics.org>
2021-05-15 15:49:24 +0800

    >  run 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  15:49:24 up 38 min,  4 users,  load average: 0.72, 1.20, 1.18 8a1599c521512c1953a00e12c327e46c200fba12

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -38,7 +38,7 @@ _RegSet* do_syscall(_RegSet *r) {
     }
     case SYS_brk:{
       Log("brk");
-      SYSCALL_ARG1(r)=0;
+      SYSCALL_ARG1(r)=sys_sbrk(SYSCALL_ARG4(r));
       break;
     }
     case SYS_read:{
