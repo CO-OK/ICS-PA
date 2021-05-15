cd9dde4e1eb610963362a58fb61f04eb06684b77
tracer-ics2017 <tracer@njuics.org>
2021-05-15 15:44:56 +0800

    >  compile 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  15:44:56 up 34 min,  4 users,  load average: 1.54, 1.44, 1.21 28b6f8ac262df946f5a1fd6b41b08facd9f0ac34

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -5,10 +5,10 @@
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
   //printf("before load\n");
-  //int fd = fs_open(filename, 0, 0);
-	//fs_read(fd, (void *)DEFAULT_ENTRY, fs_filesz(fd));
-	//fs_close(fd);
+  int fd = fs_open(filename, 0, 0);
+	fs_read(fd, (void *)DEFAULT_ENTRY, fs_filesz(fd));
+	fs_close(fd);
 	//return DEFAULT_ENTRY;
-  ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
+  //ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
