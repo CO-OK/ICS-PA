265cf7a6c652d6f6385d75d3248cdf83d2ee94ba
tracer-ics2017 <tracer@njuics.org>
2021-05-15 15:58:32 +0800

    >  run 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  15:58:32 up 47 min,  4 users,  load average: 0.52, 0.69, 0.94 4bdf71bb5bd02a443b13064c4c56087eba3f41bb

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -88,7 +88,7 @@ int sys_write(int fd, void *buf, size_t count)
     for(int i=0;i<count;i++)
     {
       _putc(((char*)buf)[i]);
-      //Log("write ch");
+      Log("write ch");
     }
     return count;
   }
