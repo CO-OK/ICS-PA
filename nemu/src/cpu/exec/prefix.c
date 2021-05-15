a0bf2cf7a3856c30feafca9e40c9311b0fe6d58a
tracer-ics2017 <tracer@njuics.org>
2021-05-15 19:54:47 +0800

    >  compile 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  19:54:47 up  4:43,  4 users,  load average: 1.47, 1.48, 1.23 6f2d0867ff314aa47999aba24fcdcd5f3cb6365c

--- nemu/src/cpu/exec/prefix.c
+++ nemu/src/cpu/exec/prefix.c
@@ -4,7 +4,7 @@ make_EHelper(real);
 
 make_EHelper(operand_size) {
   decoding.is_operand_size_16 = true;
-  //printf("eip=%08X\n",*eip);
+  printf("eip=%08X\n",*eip);
   exec_real(eip);
   decoding.is_operand_size_16 = false;
 }
