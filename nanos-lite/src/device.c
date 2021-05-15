135a60d50608469e20e6ce6e7a39d003e82eae4d
tracer-ics2017 <tracer@njuics.org>
2021-05-15 17:03:06 +0800

    >  run 1410576 v Linux v-pc 5.4.114-1-MANJARO #1 SMP PREEMPT Wed Apr 21 14:34:53 UTC 2021 x86_64 GNU/Linux  17:03:06 up  1:52,  4 users,  load average: 1.13, 0.81, 0.63 4010c42485a78fc3ca0cf338dc11ed1d69084d8

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -17,7 +17,28 @@ static char dispinfo[128] __attribute__((used));
 void dispinfo_read(void *buf, off_t offset, size_t len) {
 }
 
-void fb_write(const void *buf, off_t offset, size_t len) {
+void fb_write(const void *buf, off_t offset, size_t count) {
+      int row, col;
+      offset /= 4;
+      col = offset % _screen.width;
+      row = offset / _screen.width;
+      int total = count/4;
+      if(_screen.width-col>=total)//不止1行
+      {
+        //先写第一行
+        _draw_rect((uint32_t *)buf, col, row, _screen.width-col, 1);
+        total=total-(_screen.width-col);
+        int full_line = total / _screen.width;//可以写几个满行
+        int left_line = total % _screen.width;//最后1个不满的行所要写的数量
+        //写满行的
+        _draw_rect((uint32_t *)(buf+(_screen.width-col)*4), 0, row+1, _screen.width, full_line);
+        //写最后一行
+        _draw_rect((uint32_t *)(buf+(_screen.width-col)*4+full_line*_screen.width*4), 0, row+1+full_line, left_line, 1);
+      }
+      else
+      {
+        _draw_rect((uint32_t *)buf, col, row, total, 1);
+      }
 }
 
 void init_device() {
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -24,6 +24,8 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+  file_table[FD_FB].size=_screen.width*_screen.height*4;
+  Log("init_fs with size=%d",_screen.height*_screen.width*4);
 }
 int fs_open(const char*path,int flags,int mode)
 {
@@ -107,21 +109,55 @@ off_t fs_lseek(int fd, off_t offset, int whence)
 
 ssize_t fs_write(int fd, void *buf, size_t count)
 {
+  
+  switch(fd){
+    case FD_STDOUT:
+    case FD_STDERR:{
+      for(int i=0;i<count;i++)
+      {
+        _putc(((char*)buf)[i]);
+        //
+      }
+      break;
+    }
+    case FD_FB:{
+      //void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h)
+      //每一个元素占 4 个字节
+      if (file_table[fd].open_offset + count > fs_filesz(fd))
+		    count = file_table[fd].size - file_table[fd].open_offset;
+      int row, col;
+      int offset = file_table[FD_FB].open_offset;
+      offset /= 4;
+      col = offset % _screen.width;
+      row = offset / _screen.width;
+      int total = count/4;
+      if(_screen.width-col>=total)//不止1行
+      {
+        //先写第一行
+        _draw_rect((uint32_t *)buf, col, row, _screen.width-col, 1);
+        total=total-(_screen.width-col);
+        int full_line = total / _screen.width;//可以写几个满行
+        int left_line = total % _screen.width;//最后1个不满的行所要写的数量
+        //写满行的
+        _draw_rect((uint32_t *)(buf+(_screen.width-col)*4), 0, row+1, _screen.width, full_line);
+        //写最后一行
+        _draw_rect((uint32_t *)(buf+(_screen.width-col)*4+full_line*_screen.width*4), 0, row+1+full_line, left_line, 1);
+      }
+      else
+      {
+        _draw_rect((uint32_t *)buf, col, row, total, 1);
+      }
+      file_table[fd].open_offset+=count;
+      break;
+    }
+    default:{
+      if (file_table[fd].open_offset + count > fs_filesz(fd))
+		    count = file_table[fd].size - file_table[fd].open_offset;
+      ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, count);
+      file_table[fd].open_offset+=count;
+    }
+  }
   Log("write %d name=%s count=%d",fd,file_table[fd].name,count);
-  if (file_table[fd].open_offset + count > fs_filesz(fd))
-		count = file_table[fd].size - file_table[fd].open_offset;
-  ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, count);
-  file_table[fd].open_offset+=count;
+  
   return count;
-}
-void set_open_offset(int fd , off_t n)
-{
-  if(n>file_table[fd].size)
-    n=file_table[fd].size;
-  file_table[fd].open_offset=n;
-}
-off_t get_open_offset(int fd)
-{
- 
-  return file_table[fd].open_offset;
 }
