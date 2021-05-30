6a84bf9b87f70d6fbbe7515577f2dd4744c35237
tracer-ics2017 <tracer@njuics.org>
2021-05-30 20:30:27 +0800

    >  run 1410576 v Linux v-pc 5.4.118-1-MANJARO #1 SMP PREEMPT Tue May 11 17:46:18 UTC 2021 x86_64 GNU/Linux  20:30:27 up  6:01,  4 users,  load average: 0.91, 0.89, 0.82 942a828a6e39781a2ea3a91e42a9a94434ba4d9f

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,26 +3,23 @@
 //#define DEFAULT_ENTRY ((void *)0x4000000)
 #define DEFAULT_ENTRY ((void *)0x8048000)
 uintptr_t loader(_Protect *as, const char *filename) {
-  Log("open file");
+  //Log("open file");
   int fd = fs_open(filename,0,0);
   size_t file_size = fs_filesz(fd);
   size_t page_num = file_size / PGSIZE;
   if(file_size%PGSIZE!=0)
     page_num++;
-  Log("file_size=%d,num of page=%d",file_size,page_num);
+  //Log("file_size=%d,num of page=%d",file_size,page_num);
   void* pa;
   void* va = (void*)DEFAULT_ENTRY;
   for(int i = 0; i < page_num; i ++)
   {
     pa = new_page();
-    Log("before map");
     _map(as, va, pa);
-    Log("after map");
     fs_read(fd,pa,PGSIZE);
-    Log("fin fs_read");
     va += PGSIZE;
   }
   fs_close(fd);
-  Log("loader finished");
+  //Log("loader finished");
   return DEFAULT_ENTRY;
 }
