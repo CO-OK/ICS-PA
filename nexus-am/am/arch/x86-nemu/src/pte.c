659e49ba9bd05bb8ab21e225a32e93b21ec0d51f
tracer-ics2017 <tracer@njuics.org>
2021-05-30 20:26:32 +0800

    >  run 1410576 v Linux v-pc 5.4.118-1-MANJARO #1 SMP PREEMPT Tue May 11 17:46:18 UTC 2021 x86_64 GNU/Linux  20:26:32 up  5:57,  4 users,  load average: 0.66, 0.75, 0.75 10dbdf6822add73e64345aaf94b8a48cc6c1c70b

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -67,20 +67,8 @@ void _switch(_Protect *p) {
 
 void _map(_Protect *p, void *va, void *pa) {
   //功能是将虚拟地址空间 p 中的虚拟地址 va 映射到物理地址 pa
-  uint32_t* ptr = (uint32_t*)p->ptr;
-  uint32_t shift = (uintptr_t)va >> 22;
-  uintptr_t tr = ptr[shift];
-  if (tr == kpdirs[shift]) 
-  {
-      PTE *uptable = (PTE*)(palloc_f());
-      ptr[shift] = (uintptr_t)uptable | PTE_P;
-  }
-  tr = ptr[shift];
-  shift = (((uintptr_t)va) & 0x003ff000) >> 12;
-  uint32_t* pgr = (uint32_t*)(tr & 0xfffff000);
-  pgr[shift] = (uintptr_t)pa | PTE_P;
-  //return 0;
-  /*uint32_t* page_dir_base = (uint32_t*)p->ptr;
+  
+  uint32_t* page_dir_base = (uint32_t*)p->ptr;
   uint32_t page_dir_index = (uintptr_t)va >> 22;
   uintptr_t page_table_base = *(page_dir_base+page_dir_index);
   if(!(page_table_base&PTE_P))//不存在二级页表
@@ -90,7 +78,6 @@ void _map(_Protect *p, void *va, void *pa) {
   page_table_base = *(page_dir_base+page_dir_index);
   uint32_t page_table_index = (((uintptr_t)va) & 0x003ff000) >> 12;
   *((uint32_t*)(page_table_base & 0xfffff000)+page_table_index)=(uintptr_t)pa | PTE_P;
-  return 0;*/
 }
 
 void _unmap(_Protect *p, void *va) {
