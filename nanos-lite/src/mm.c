b11bac79e37045e30aed082e8bd40ae4a5c7df28
tracer-ics2017 <tracer@njuics.org>
2021-05-30 21:03:52 +0800

    >  run 1410576 v Linux v-pc 5.4.118-1-MANJARO #1 SMP PREEMPT Tue May 11 17:46:18 UTC 2021 x86_64 GNU/Linux  21:03:52 up  6:34,  4 users,  load average: 1.01, 0.77, 0.67 4597b5be37954634e2d601170d8a54c5606ea73

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -25,7 +25,7 @@ int mm_brk(uint32_t new_brk) {
   {
     if(new_brk>current->max_brk)
     {
-      uintptr_t va = (current->max_brk & 0xfffff000);
+      /*uintptr_t va = (current->max_brk & 0xfffff000);
       while(va < (uint32_t)(new_brk & 0xfffff000))
       {
         void* pa = new_page();
@@ -33,8 +33,8 @@ int mm_brk(uint32_t new_brk) {
         va += PGSIZE;
         // Log("brk %x, %x\n", va, pa);
       }
-      current->max_brk = new_brk;
-      /*uint32_t first = PGROUNDUP(current->max_brk);
+      current->max_brk = new_brk;*/
+      uint32_t first = (current->max_brk & 0xfffff000);
       uint32_t end=PGROUNDDOWN(new_brk);
       if((new_brk&0xfff)==0)
       {
@@ -45,7 +45,7 @@ int mm_brk(uint32_t new_brk) {
         void* pa=new_page();
         _map(&(current->as),(void*)va,pa);
       }
-      current->max_brk=new_brk;*/
+      current->max_brk=new_brk;
     }
     current->cur_brk=new_brk;
   }
