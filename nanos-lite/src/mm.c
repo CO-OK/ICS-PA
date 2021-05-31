b7241b4633f029e1679ee5bc29dd11ba0d7001bc
tracer-ics2017 <tracer@njuics.org>
2021-05-31 20:46:50 +0800

    >  run 1410576 v Linux v-pc 5.4.118-1-MANJARO #1 SMP PREEMPT Tue May 11 17:46:18 UTC 2021 x86_64 GNU/Linux  20:46:50 up  4:39,  5 users,  load average: 0.46, 0.30, 0.36 5f4c660dc8875f99e73788427be0b3e1639f2aea

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -25,13 +25,10 @@ int mm_brk(uint32_t new_brk) {
   {
     if(new_brk>current->max_brk)
     {
-      uint32_t first = PGROUNDUP(current->max_brk);
-      uint32_t end = PGROUNDDOWN(new_brk);
-      if((new_brk&0xfff)==0)//end按页对齐了，不需要新增
-      {
-        end-=PGSIZE;
-      }
-      for(uint32_t va=first;va<=end;va+=PGSIZE)
+      uint32_t up = PGROUNDUP(current->max_brk);
+      uint32_t down = PGROUNDDOWN(new_brk);
+      
+      for(uint32_t va=up;va<=down;va+=PGSIZE)
       {
         void* pa=new_page();
         _map(&(current->as),(void*)va,pa);
