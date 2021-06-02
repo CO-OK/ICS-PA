8b23a11b4bac6228417295e45e97b624506b8ca9
tracer-ics2017 <tracer@njuics.org>
2021-06-02 20:21:32 +0800

    >  compile 1410576 v Linux v-pc 5.4.118-1-MANJARO #1 SMP PREEMPT Tue May 11 17:46:18 UTC 2021 x86_64 GNU/Linux  20:21:32 up  2:09,  5 users,  load average: 0.44, 0.37, 0.41 b191c12f4befbc89d6a4020fb77c99abb1c4600

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -2,21 +2,9 @@
 #include "memory/mmu.h"
 
 void raise_intr(uint8_t NO, vaddr_t ret_addr) {
-  /* TODO: Trigger an interrupt/exception with ``NO''.
-   * That is, use ``NO'' to index the IDT.
-   */
-
-  //TODO();
-  /*
-    依次将 EFLAGS,CS,EIP 寄存器的值压入堆栈
-    从 IDTR 中读出 IDT 的首地址
-    根据异常(中断)号在 IDT 中进行索引,找到一个门描述符
-    将门描述符中的 offset 域组合成目标地址
-    跳转到目标地址
-  */
   //Log("hit raise_intr with NO=%X",NO);
   rtl_push(&cpu.EFLAGS_,4);
-  e_IF=false;
+  e_IF=false;//拒绝响应中断
   rtl_push(&cpu.cs,4);
   rtl_push(&ret_addr,4);
   vaddr_t gate_enrty= cpu.idtr_base + sizeof(GateDesc)*NO;
